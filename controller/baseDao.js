48d5ba38328cf3f5c15f34320a94266d93f13555
ma <921883138@qq.com>
2017-04-16 09:12:21 +0800

    c

--- controller/baseDao.js
+++ controller/baseDao.js
@@ -9,14 +9,31 @@ base.save = function (bean,obj) {
     var baseinfo = new Info(obj);
     baseinfo.save();
 }
-//查询obj
+//查询obj一个
 base.findOne = function (bean,obj,callback) {
     var Info = require('./../model/'+bean);
     Info.findOne(obj,callback);
 }
-
-
-
+//it查询
+base.findbyId = function (bean,id,callback) {
+    var Info = require('./../model/'+bean);
+    Info.findbyId(id,callback);
+}
+//修改
+base.update = function (bean,obj,update,callback) {
+    var Info = require('./../model/'+bean);
+    Info.update(obj,update,callback);
+}
+//删除
+base.remove = function (bean,obj,callback) {
+    var Info = require('./../model/'+bean);
+    Info.remove(obj,callback)
+}
+//过滤查询
+base.find = function (bean,obj,callback) {
+    var Info = require('./../model/'+bean);
+    Info.find(obj,callback);
+}
 
 
 
